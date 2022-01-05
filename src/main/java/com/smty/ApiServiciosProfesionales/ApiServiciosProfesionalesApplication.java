057ffd54c59030d808bfdbb39da233f488fb1694
delacruz <ldelacruzg@uteq.edu.ec>
2022-01-05 12:03:54 -0500

    Inicialización del proyecto, creación de las carpetas bases y configuración de la base de datos

---
+++ src/main/java/com/smty/ApiServiciosProfesionales/ApiServiciosProfesionalesApplication.java
@@ -0,0 +1,13 @@
+package com.smty.ApiServiciosProfesionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiciosProfesionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiciosProfesionalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smty/ApiServiciosProfesionales/ApiServiciosProfesionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smty.ApiServiciosProfesionales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiciosProfesionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
