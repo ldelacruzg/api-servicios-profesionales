99588eefa948ff540868d602ecf1b3ebe31b9d20
delacruz <ldelacruzg@uteq.edu.ec>
2022-01-05 12:27:18 -0500

    Creación del modelo Profesional

---
+++ src/main/java/com/smty/ApiServiciosProfesionales/Models/Profesional.java
@@ -0,0 +1,10 @@
+package com.smty.ApiServiciosProfesionales.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "profesionales")
+public class Profesional {
+   
+}
