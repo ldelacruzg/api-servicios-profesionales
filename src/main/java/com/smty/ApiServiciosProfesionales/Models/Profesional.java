450f1a2a286402ce06c70de268cbae98cb8c1f41
Jonathan Ptricio Cevallos Mayorga <jonathancevv@gmail.com>
2022-01-05 14:48:08 -0500

    prueba commit

--- src/main/java/com/smty/ApiServiciosProfesionales/Models/Profesional.java
+++ src/main/java/com/smty/ApiServiciosProfesionales/Models/Profesional.java
@@ -1,11 +1,13 @@
 package com.smty.ApiServiciosProfesionales.Models;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "profesionales")
-public class Profesional {
+public class Profesional implements Serializable {
    private Long idProfesional;
    private String urlSitioWeb;
 }
