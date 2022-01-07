31886cf0953537fe1586695b68b0d0be5f747bf1
Jonathan Ptricio Cevallos Mayorga <jonathancevv@gmail.com>
2022-01-07 14:11:36 -0500

    commit

--- src/main/java/com/smty/ApiServiciosProfesionales/Repositories/ProfesionalRepository.java
+++ src/main/java/com/smty/ApiServiciosProfesionales/Repositories/ProfesionalRepository.java
@@ -2,6 +2,8 @@ package com.smty.ApiServiciosProfesionales.Repositories;
 
 import com.smty.ApiServiciosProfesionales.Models.Profesional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfesionalRepository extends JpaRepository<Profesional,Long> {
 }
