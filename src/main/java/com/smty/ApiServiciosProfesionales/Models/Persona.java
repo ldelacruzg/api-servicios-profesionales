8a21a77088aa2fd046c332c388a83bd11273ab34
Jonathan Ptricio Cevallos Mayorga <jonathancevv@gmail.com>
2022-01-15 10:37:56 -0500

    Se actualizo la clase Persona.java

--- src/main/java/com/smty/ApiServiciosProfesionales/Models/Persona.java
+++ src/main/java/com/smty/ApiServiciosProfesionales/Models/Persona.java
@@ -3,6 +3,8 @@ package com.smty.ApiServiciosProfesionales.Models;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Entity
 @Table(name = "personas")
@@ -12,16 +14,19 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_persona")
     private Long idPersona;
-	
+
+	@NotEmpty
 	@Column(name="nombre", length = 100)
     private String nombre;
-	
+
+	@NotEmpty
 	@Column(name="apellido", length = 100)
     private String apellido;
 	
 	@Column(name="telefono", length = 15)
     private String telefono;
-	
+
+	@NotEmpty
 	@Column(name="direccion")
     private String direccion;
 	
@@ -30,16 +35,19 @@ public class Persona {
 	
 	@Column(name="descripcion")
     private String descripcion;
-	
+
+
 	@Column(name="username", length = 20)
     private String username;
-	
+
 	@Column(name="clave", length = 15)
     private String clave;
 
+	@NotEmpty
 	@Column(name="dni", length = 15)
 	private String dni;
 
+	@NotEmpty
 	@Column(name="email", length = 50)
 	private String email;
 	
